54e9216f22e71844efbaf4864cc29490eb13c0a4
Zyz <zuzz001zu@gmail.com>
2026-01-26 12:24:15 +0300

    done

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -9,6 +9,9 @@ namespace BankAccountNS
     {
         private readonly string m_customerName;
         private double m_balance;
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
 
         private BankAccount() { }
 
@@ -32,15 +35,16 @@ namespace BankAccountNS
         {
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
-            m_balance += amount;
+
+            m_balance -= amount;
         }
 
         public void Credit(double amount)
